b85b5b96bffcbd69dd19aeffd75fba16d11e08d6
Advith Chelikani <advith.chelikani@gmail.com>
2021-03-06 23:18:25 -0600

    restructure files

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import secrets
-from greenhouse_client import GreenhouseClient
-from slack_client import SlackClient
+from clients.greenhouse_client import GreenhouseClient
+from clients.slack_client import SlackClient
 
 # Set env variables for API tokens.
 secrets.set_tokens()
