6299dc523f2c74c692347b9d1925e868871b5edd
Advith Chelikani <advith.chelikani@gmail.com>
2021-04-19 19:49:45 -0500

    add lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -1,15 +1,14 @@
-import secrets
-from config.affinity_test import AffinityTest
 from services.applicationwatcherservice.all_orgs_application_watcher import (
     AllOrgsApplicationWatcher,
 )
+from config.affinity_test import AffinityTest
 from datetime import date
+import json
 
-# Set env variables for API tokens.
-secrets.set_tokens()
 
-if __name__ == "__main__":
+def lambda_handler(event, context):
     configs = [AffinityTest]
     job = AllOrgsApplicationWatcher(configs)
     today = date.today().isoformat()
     job.run(today)
+    return {"statusCode": 200, "body": json.dumps("Success")}
