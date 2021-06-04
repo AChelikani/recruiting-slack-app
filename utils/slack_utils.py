58c31ce20fbaecc36441e886880d05003c5785da
Advith Chelikani <advith.chelikani@gmail.com>
2021-06-03 21:22:56 -0500

    don't allow special chars in channel name

--- utils/slack_utils.py
+++ utils/slack_utils.py
@@ -1,7 +1,21 @@
 def generate_new_onsite_channel_name(first_name, last_name, date):
+    first_name = first_name.lower().replace(" ", "")
+    last_name = last_name.lower().replace(" ", "")
+
+    allowed_char_set = "abcdefghijklmnopqrstuvwxyz0123456789-_"
+    cleaned_first_name = ""
+    cleaned_last_name = ""
+    for char in first_name:
+        if char in allowed_char_set:
+            cleaned_first_name += char
+
+    for char in last_name:
+        if char in allowed_char_set:
+            cleaned_last_name += char
+
     return "{}-{}-{}-{}".format(
         date,
         "onsite",
-        first_name.lower().replace(" ", ""),
-        last_name.lower().replace(" ", ""),
+        cleaned_first_name,
+        cleaned_last_name,
     )
